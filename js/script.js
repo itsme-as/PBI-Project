9ee1303cedf6a874101e7eeb84a6bf6cd387c879
Alief Maulana Shiddiq <aliefshiddiq0@gmail.com>
2025-07-15 15:25:28 +0700

    Project PBI by Luarsekolah

---
+++ js/script.js
@@ -0,0 +1,19 @@
+let index = document.querySelectorAll('.index-text');
+let currentIndex = 0;
+
+ setInterval(() => {
+     index[currentIndex].classList.remove('active-index');
+     currentIndex = (currentIndex + 1) % index.length;
+     index[currentIndex].classList.add('active-index');
+ }, 2000);
+
+//  Script untuk menghilangkan form dan muncul text
+function handleSubmit(event) {
+    event.preventDefault(); // Mencegah form reload halaman
+
+    // Sembunyikan form
+    document.querySelector('.msg-form').style.display = 'none';
+
+    // Tampilkan ucapan terima kasih
+    document.getElementById('thankyou-msg').style.display = 'block';
+}
